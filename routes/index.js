a4661cfe3546ab7830e30ea42adea2356719e0e7
EMENCII <ciarrocca.marco@gmail.com>
2017-04-03 14:53:40 +0200

    struttura db, rotte

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
 router.get('/registrazione', function(req, res, next) {
   res.render('registrazione');
 });
 
+router.get('/builds', function(req, res, next) {
+  res.render('builds');
+});
+
+router.get('/chat', function(req, res, next) {
+  res.render('chat');
+});
+
+router.get('/community', function(req, res, next) {
+  res.render('community');
+});
 
 module.exports = router;
