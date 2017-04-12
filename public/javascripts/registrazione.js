48f648a070c97c2c584c2dfd6d9d7da27f647f5e
Unknown <ciarrocca.marco@gmail.com>
2017-04-12 13:01:12 +0200

    errors

--- public/javascripts/registrazione.js
+++ public/javascripts/registrazione.js
@@ -2,8 +2,13 @@ jQuery(function($) {
   var socket = io();
 
   $('#sub').click(function(){
+    $('#nickdiv').removeClass('has-error has-feedback');
+    $('#emaildiv').removeClass('has-error has-feedback');
     socket.on('risultato',function(data){
-      alert(data.messaggio);
+      switch(data.cod){
+        case 0: $('#nickdiv').addClass('has-error has-feedback'); break;
+        case 1: $('#emaildiv').addClass('has-error has-feedback'); console.log(data.cod); break;
+      }
     });
     socket.emit('registrazione', {
         nome: $('#nome').val(),
@@ -11,6 +16,19 @@ jQuery(function($) {
         email: $('#email').val(),
         username: $('#nick').val(),
         password: $('#pw').val()
+    });
+  });
+
+  $('#navlog').click(()=>{
+    socket.emit('login',{
+      usr: $('#navusr').val(),
+      pw: $('#navpw').val()
+    });
+    socket.on('loginerror', (data)=>{
+      switch(data.cod){
+        case 0: $('#log').addClass('has-error has-feedback'); break;
+        case 1: $('#log1').addClass('has-error has-feedback'); console.log(data.cod); break;
+      }
     });
   });
 });
